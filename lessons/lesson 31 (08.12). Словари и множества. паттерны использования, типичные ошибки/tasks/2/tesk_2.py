193e781f05fef848c5f761b39142f3d0abc71bdd
VeronicaNik <nika.niki02@mail.ru>
2025-12-08 20:23:19 +0500

    Create tesk_2.py

---
+++ lessons/lesson 31 (08.12). Словари и множества. паттерны использования, типичные ошибки/tasks/2/tesk_2.py
@@ -0,0 +1,6 @@
+rides_by_driver = []
+shift_log = rides_by_driver.get()
+shift_log = [('drv-1', 6), ('drv-2', 4), ('drv-1', 5), ('drv-3', 2)]
+finished = ['drv-2']
+shift_log = rides_by_driver.pop()
+print(shift_log )
