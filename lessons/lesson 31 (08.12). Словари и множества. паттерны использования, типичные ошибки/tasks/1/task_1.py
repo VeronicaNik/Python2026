1fa087da3d64399c431a5f5609f06b9831961276
VeronicaNik <nika.niki02@mail.ru>
2025-12-08 20:09:06 +0500

    Create task_1.py

---
+++ lessons/lesson 31 (08.12). Словари и множества. паттерны использования, типичные ошибки/tasks/1/task_1.py
@@ -0,0 +1,7 @@
+tours = {
+    ('Iceland', 'Лена'),
+    ('Georgia', 'Тимофей'),
+    ('Mongolia', 'Аделина'),}
+request_tour = tours.get('Georgia','Тимофей')
+new_offer = tours.get('Chile','не занесен')
+print(tours) 
