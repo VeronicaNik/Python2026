3f15d4b312133133f8905424304dad7b8b9d4557
VeronicaNik <nika.niki02@mail.ru>
2025-12-08 20:07:54 +0500

    Delete lessons/lesson 31 (08.12). Словари и множества. паттерны использования, типичные ошибки/tasks/1/task1.py

--- lessons/lesson 31 (08.12). Словари и множества. паттерны использования, типичные ошибки/tasks/1/task1.py
+++
@@ -1,8 +0,0 @@
-editor = {}
-tours = [
-    ('Iceland', 'Лена'),
-    ('Georgia', 'Тимофей'),
-    ('Mongolia', 'Аделина'),]
-request_tour = tours.get('Georgia','Тимофей')
-new_offer = tours.get('Chile','не занесен')
-print(tours)
